30e325aef321f43747eebc29192eb5cd6fc35b7f
Atul <atulfind@gmail.com>
2023-07-22 11:45:03 +0530

    console message

---
+++ IIFE.js
@@ -0,0 +1,9 @@
+const title = ((action, type, subType) => {
+    if (action === "create") {
+      return `Add ${_.startCase(type)}`;
+    } 
+      return `Edit ${_.startCase(type)}`; // this is change || sujeet change || yes || oyeee
+    
+  })(action, "Practical Exam", subType);
+
+  console.log("hi Atul")
