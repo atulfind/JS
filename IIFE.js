37e93e18b33a4ddd35dc8c754aed60be2376f2dd
Sujeet Kumar <116361469+rsujeet@users.noreply.github.com>
2023-07-22 11:18:02 +0530

    Update IIFE.js

--- IIFE.js
+++ IIFE.js
@@ -2,6 +2,6 @@ const title = ((action, type, subType) => {
     if (action === "create") {
       return `Add ${_.startCase(type)}`;
     } 
-      return `Edit ${_.startCase(type)}`; // this is change || sujeet change
+      return `Edit ${_.startCase(type)}`; // this is change || sujeet change || no
     
   })(action, "Practical Exam", subType);
