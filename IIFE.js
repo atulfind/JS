54be33a81bea246e18a84ca720c43c10c1e26249
Sujeet Kumar Rajpooot <rsujeet85@gmail.com>
2023-07-22 11:15:29 +0530

    comment add

--- IIFE.js
+++ IIFE.js
@@ -2,6 +2,6 @@ const title = ((action, type, subType) => {
     if (action === "create") {
       return `Add ${_.startCase(type)}`;
     } 
-      return `Edit ${_.startCase(type)}`; // this is change
+      return `Edit ${_.startCase(type)}`; // this is change || sujeet change
     
   })(action, "Practical Exam", subType);
